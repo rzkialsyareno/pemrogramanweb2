a417387d5d0ec61c164dc91ade2c7a552371e62a
Rizki Alsyareno <rzkialsyareno@gmail.com>
2024-11-21 10:54:32 +0700

    Toko Buku Online - 2

--- toko-buku-online/app/Controllers/Home.php
+++ toko-buku-online/app/Controllers/Home.php
@@ -8,4 +8,24 @@ class Home extends BaseController
     {
         return view('home');
     }
+
+    public function chart(): string
+    {
+        return view('chart');
+    }
+
+    public function checkout(): string
+    {
+        return view('checkout');
+    }
+
+    public function search(): string
+    {
+        return view('search');
+    }
+
+    public function submit(): string
+    {
+        return view('submit');
+    }
 }
